de07c7d8e0dc90ebc0ac76fc748cd9bdcc655779
water-eagle <130212192+water-eagle@users.noreply.github.com>
2025-12-01 10:42:50 +0900

    앱 프로그래밍 기초 11주차 실습 응용

--- Flutter/bamtol_market_app/lib/app.dart
+++ Flutter/bamtol_market_app/lib/app.dart
@@ -14,9 +14,17 @@ class _AppState extends State<App> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('lib/assets/images/logo.jpeg'),
-            SizedBox(height: 20), // 이미지와 텍스트 사이 간격
-            Text('This is Logo'),
+            Image.asset('./assets/images/logo_flock.jpeg'),
+            /*SizedBox(height: 20), // 이미지와 텍스트 사이 간격
+            Text(
+              'This is Logo',
+              style: TextStyle(
+                fontSize: 24, // 글꼴 크기 (기본 단위: 픽셀)
+                fontWeight: FontWeight.bold, // 굵기 (bold, normal 등)
+                color: Colors.blue, // 색상
+                fontStyle: FontStyle.italic, // 기울임 (선택 사항)
+              ),
+            ),*/
           ],
         ),
       ),
--- Flutter/bamtol_market_app/lib/main.dart
+++ Flutter/bamtol_market_app/lib/main.dart
@@ -18,6 +18,13 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: '당근마켓 클론 코딩',
       initialRoute: '/',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          backgroundColor: Color(0xff212123),
+          titleTextStyle: TextStyle(color: Colors.white),
+        ),
+      ),
       getPages: [GetPage(name: '/', page: () => const App())],
     );
   }
